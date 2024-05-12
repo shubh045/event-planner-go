517fe9d8d4997a1cb3fcdf65a8b338d5ebce3ed0
Shubham <shubh6441@gmail.com>
2024-05-12 16:26:05 +0530

    protect routes

--- routes/events.go
+++ routes/events.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 
 	"example.com/event-planner/models"
-	"example.com/event-planner/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -22,28 +21,15 @@ func getEvents(context *gin.Context) {
 
 func createEvent(context *gin.Context) {
 
-	token := context.Request.Header.Get("Authorization")
-
-	if token == "" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized!"})
-		return
-	}
-
-	userId, err := utils.VerifyToken(token)
-
-	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized!"})
-		return
-	}
-
 	var event models.Event
-	err = context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data."})
 		return
 	}
 
+	userId := context.GetInt("userId")
 	event.UserId = userId
 
 	err = event.Save()
